12ef9f8f83187f3b5b33ffd9ab97363ce1339800
cf-eli <chandlerfletcher99@gmail.com>
2021-08-27 01:17:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import yfinance as yf
+import json
 
-msft = yf.Ticker("MSFT")
+#msft = yf.Ticker("MSFT")
 # for key, value in msft.info.items():
 #     print(key)
-print(msft.info["currentPrice"])
+#print(msft.info["currentPrice"])
 #hist = msft.history(period="max")
 #print(hist)
 
+def getData():
+    try:
+        with open("data.json", "r") as f:
+            data = json.load(f)
+        return data.items()
+    except FileNotFoundError:
+        data = {'no': 'test', 'yes': 'test2'}
+    return data
+class accountData():
+    def __init__ (self, name, info):
+        self.name = name
+        self.info = info
 def checkPrice():
     return 1
 def buyStock():
@@ -17,14 +30,33 @@ def sellStock():
     return 1
 def checkHistory():
     return 1
-def loadAccount():
-    print("Current lists of accounts: ")
+
+def loadAccount(): #load user account or create new accounts
+    def accountName(data, name):
+        print(data)
+        for k, v in data.items():
+            if k == name:
+                return accountData(k, v)
+        data[name] = {}
+        return accountData(name, data)
+        #balance = input("How much cash would you like for the account to start with?") #finish this
+    data = getData()
+    if data:
+        print("Current lists of accounts: ")
+        for name2 in data:
+            print(f"{name2} ")
+        name = input("Enter your account name to log into your account or enter an unique name to create a new account: ")
+        acc = accountName(data, name)
+    else:
+        name = input("You currently have no accounts. Enter an account name: ")
+        acc = accountName(data, name)
+    print(f"Current account is {acc.name}")
     #write code that display list of account here
-    print("Enter your account name to log into your account or enter an unique name to create a new account.")
-def exit():
+
+def exit(): #quit program
     return 1
 
-def menu():
+def menu(): #ui 
     loadAccount()
     "Enter a number that correspond with what you wish to do."
     commandList = [["Find more information about a ticker", checkPrice], ["Buy some stock", buyStock], ["Check your current stocks", checkStock],
@@ -39,3 +71,7 @@ def menu():
         commandList[function][1]()
 
 menu()
+
+
+#{bobyolo: {Netflix: { 294.2: 5}{295: 2}}{Snapchat: {482.2: 1}{384.4: 4}}}{bobdiverfsnied: {MSFT: {900: 439}}} 
+#^ currently missing balance
